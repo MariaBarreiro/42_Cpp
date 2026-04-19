b70a6c3a75dbbb076867dbb589426bc348b3bf9b
Maria Barreiro <mariaeobarreiro@gmail.com>
2026-04-19 11:12:46 +0100

    added Makefile

--- cpp00/ex01/inc/Contact.hpp
+++ cpp00/ex01/inc/Contact.hpp
@@ -1,6 +1,13 @@
 #ifndef CONTACT_HPP
 #define CONTACT_HPP
 
+#define BLUE	"\033[38;2;59;57;93m"
+#define PURPLE	"\033[38;2;144;133;188m"
+#define LPINK	"\033[38;2;242;203;224m"
+#define PINK	"\033[38;2;203;109;154m"
+#define DPINK "\033[38;2;143;71;123m"
+#define RES "\033[0m"
+
 #include <string>
 
 class Contact {
@@ -21,9 +28,9 @@ public:
   void setPhoneNumber(std::string phoneNumber);
   void setDarkestSecret(std::string darkestSecret);
 
-	std::string	getFirstName();
-	std::string	getLastName();
-	std::string	getNickname();
+  std::string getFirstName();
+  std::string getLastName();
+  std::string getNickname();
 };
 
 #endif
