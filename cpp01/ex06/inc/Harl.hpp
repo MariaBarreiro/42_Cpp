630ec532a784ecdabe82d86a457246ac57dce953
Maria Barreiro <mariaeobarreiro@gmail.com>
2026-06-27 16:31:38 +0100

    finished cpp01

---
+++ cpp01/ex06/inc/Harl.hpp
@@ -0,0 +1,27 @@
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#define BLUE	"\033[38;2;78;76;118m"
+#define PURPLE	"\033[38;2;144;133;188m"
+#define LPINK	"\033[38;2;242;203;224m"
+#define PINK	"\033[38;2;203;109;154m"
+#define DPINK "\033[38;2;143;71;123m"
+#define RES "\033[0m"
+
+#include <string>
+#include <iostream>
+
+class	Harl {
+public:
+	Harl();
+	~Harl();
+	void	complain(std::string level);
+
+private:
+	void	debug(void);
+	void	info(void);
+	void	warning(void);
+	void	error(void);
+};
+
+#endif
